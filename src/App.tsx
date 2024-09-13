26b985aada9369fe08234825bbdb4fd423159eeb
Chanyoung Park <rhdwkrxla68@gmail.com>
2024-09-13 23:44:25 +0900

    Remove: 안쓰는 코드 라인 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,6 @@
-import { Fragment, useEffect } from "react";
 import { SplashScreen } from "./pages/SplashScreen";
 
 function App() {
-  
-  // useEffect(() => {
-  //   window.location.href = 'https://tossinvest.com/';
-  // }, []);
-
   return (
     <SplashScreen />
   );
