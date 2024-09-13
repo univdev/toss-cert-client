3886b8b3a9255fb418c5e2a67a9acbf83d4c8324
Chanyoung Park <rhdwkrxla68@gmail.com>
2024-09-13 21:49:50 +0900

    Chore: 프로젝트 설정갑 수정
    - mac os 빌드를 위한 설정값 추가

--- apps/client/main.js
+++ apps/client/main.js
@@ -1,9 +1,13 @@
 import { app, BrowserWindow } from 'electron';
+import { join, resolve } from 'path';
+
+const __dir = resolve();
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 1366,
     height: 960,
+    icon: join(__dir, '/build/images/toss-icon.png'),
   })
 
   win.loadURL('https://tossinvest.com/')
